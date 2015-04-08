438e86d9c7bb126bfd8fed8fcd3328678d6e4833
Steven Goodgrove <steven@goodgrove.co.uk>
2015-04-08 09:07:31 +0100

    Typo.

--- Solutions/Exercise 12 - Http Uppercase.js
+++ Solutions/Exercise 12 - Http Uppercase.js
@@ -1,6 +1,5 @@
 var http = require('http');
 var map = require('through2-map');
-var url = require('url');
 
 var listenPort = process.argv[2];
 
@@ -8,7 +7,7 @@ var server = http.createServer();
 
 server.on('request', function(message, response) {
 	if (message.method != "POST") {
-		repsonse.statusCode = 500;
+		response.statusCode = 500;
 		response.end();
 		}
 	else {
